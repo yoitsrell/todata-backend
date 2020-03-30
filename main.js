77c599140b2f7fbc6fe1dc0d0f849f3dce1958bf
Tyrell Cotton <tyrell.cotton@codeimmersives.com>
2020-03-29 22:56:29 -0400

    Initial

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 /********************
  * HELPER FUNCTIONS *
  ********************/
-const getTodoName = function() {}
-const getCompleteness = function() {}
-const getPriority = function() {}
-const isComplete = function() {}
-const isHighPriority = function() {}
+const getTodoName = function(todo) {
+  return todo.text
+}
+const getCompleteness = function(todo) {
+  return todo
+}
+const getPriority = function(todo) {
+  return todo.priority
+}
+const isComplete = function(todo) {
+  return todo.complete
+}
+const isHighPriority = function(todo) {
+  return todo.priority > 1
+}
 
 /***********************
  * ITERATION FUNCTIONS *
  ***********************/
-const names = function() {}
-const priorities = function() {}
-const namesAndPriorities = function() {}
+const names = function(array) {
+  return array.map(getTodoName)
+}
+const priorities = function(array) {
+  return array.map(getPriority)
+}
+const namesAndPriorities = function(array) {
+  return array.map(getTodoName + ' ' + getPriority)
+}
 const justNotComplete = function() {}
 const justComplete = function() {}
 const priority2Only = function() {}
